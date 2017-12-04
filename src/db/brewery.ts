f202becd55b1ae1f1de464ec3ad5c0b20b25a87f
Tim Myers <tmyers@closetbox.me>
2017-12-03 20:14:10 -0700

    change ID to id

--- src/db/brewery.ts
+++ src/db/brewery.ts
@@ -9,7 +9,7 @@ export const getBreweries = async () => {
   const breweries = await breweryInit();
   const ret = await breweries.find().toArray();
   return ret.map((brewery) => {
-    brewery.ID = brewery._id;
+    brewery.id = brewery._id;
     delete brewery._id;
     return brewery;
   });
